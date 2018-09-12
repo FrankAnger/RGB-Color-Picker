4ca16d2c88621ca17eb401f4367c6e87452ca198
Constantine <TheFrankAnger@gmail.com>
2018-09-12 22:02:23 +0300

    Added custom Toolbar, and new colors

--- app/src/main/java/com/example/kosti/rgb/MainActivity.java
+++ app/src/main/java/com/example/kosti/rgb/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.kosti.rgb;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.graphics.Color;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.content.ClipboardManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     private SeekBar seek1;
     private SeekBar seek2;
@@ -23,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.button);
+
         seek1 = findViewById(R.id.seek1);
         seek2 = findViewById(R.id.seek2);
         seek3 = findViewById(R.id.seek3);
@@ -32,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
         seek1.setOnSeekBarChangeListener(new displayRGB());
         seek2.setOnSeekBarChangeListener(new displayRGB());
         seek3.setOnSeekBarChangeListener(new displayRGB());
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+
+        button.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("", text.getText());
+                clipboard.setPrimaryClip(clip);
+
+                Context context = getApplicationContext();
+                CharSequence text = "Saved to Clipboard";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
 
     }
 
